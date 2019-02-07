be4599e6745f3b44ed69a10c7d56177b0468f875
steveoh <sgourley@utah.gov>
2019-02-06 21:52:17 -0700

    feat: replace hello world with elected official flow

--- lambda/py/hello_world.py
+++ lambda/py/hello_world.py
@@ -5,25 +5,51 @@ import requests
 from flask import Flask
 from flask_ask import Ask, request, session, question, statement
 
-
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
+
 @ask.launch
 def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = (
+        'Welcome to the Utah Voting Information Assistant. ' + 'To find out who your state elected officials are, say, Who are my state elected officials?'
+    )
 
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
+    return question(speech_text).reprompt(speech_text).simple_card('ElectedOfficials', speech_text)
+
+
+@ask.intent('ElectedOfficialsIntent')
+def elected_officials():
+    speech_text = (
+        'Your representative is Democrat Patrice M. Arent ' + 'and your Senator is Democrat Jani Iwamoto. Would you like more information ' +
+        'about your Senator or Representative? Say, senator or representative.'
+    )
+
+    return question(speech_text).simple_card('ElectedOfficials', speech_text)
+
+
+@ask.intent('ElectedOfficialsDetailsIntent')
+def elected_officials_details(official):
+    if official is None:
+        return question('unknown')
+
+    if official == 'representative':
+        speech_text = '''Jani has straight hair. Would you like to call her? Say, call my representative.'''
+    elif official == 'senator':
+        speech_text = '''Patrice has curly hair. Would you like to call her? Say, call senator.'''
+    else:
+        speech_text = '''unknown'''
+
+    return question(speech_text).simple_card('ElectedOfficialsDetails', speech_text)
+
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
     speech_text = 'You can say hello to me!'
+
     return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
 
 @ask.session_ended
 def session_ended():
