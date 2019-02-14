c69b45b703198cf692d3b532f2ff8a31ea4f29ba
steveoh <sgourley@utah.gov>
2019-02-13 18:49:28 -0700

    fix: add better instructions for adding an address

--- lambda/py/vote_skill.py
+++ lambda/py/vote_skill.py
@@ -38,7 +38,10 @@ PERMISSIONS = ['alexa::devices:all:address:full:read']
 WELCOME = 'Welcome to the Utah Voting Information Assistant. Ask your question or ask for help to be guided through the questions I can answer.'
 WHAT_DO_YOU_WANT = 'Ask your question or ask for help to be guided through the questions I can answer.'
 NOTIFY_MISSING_PERMISSIONS = 'Please enable Location permissions in the Amazon Alexa app.'
-NO_ADDRESS = 'It looks like you do not have an address set. You can set your address from the companion app.'
+NO_ADDRESS = (
+    'It looks like you do not have an address set. You can set your address from the companion app. '
+    'Go to settings, then device settings, find the device you are interacting with and finally select location to add an address'
+)
 ERROR = 'Uh Oh. Looks like something went wrong.'
 AGRC_API_ERROR = 'Make sure your address is in Utah and is valid and try again.'
 
