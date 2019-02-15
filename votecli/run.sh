9b31542cf9a3a65f26a9307606ba14fa6301ffe3
steveoh <sgourley@utah.gov>
2019-02-14 22:37:17 -0700

    chore: remove testing statements

--- votecli/run.sh
+++ votecli/run.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-echo "current working directory is $(pwd)"
-echo "package root is $(dirname "$0")/$(dirname $(readlink "$0"))"
 exec node --experimental-modules --no-warnings "$(dirname "$0")/$(dirname $(readlink "$0"))/main.mjs" "$@"
