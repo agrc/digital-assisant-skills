89293bd1fdcbf82997e9572dc4b66430ceb036ff
steveoh <sgourley@utah.gov>
2019-02-19 19:32:02 -0700

    feat: update recoding skillId via cli

--- votecli/main.mjs
+++ votecli/main.mjs
@@ -1,10 +1,11 @@
 'use strict';
 
-import { getFirstAnswers, getTagAnswers, getBootstrapAnswers, entries } from './questions.mjs';
+import { getFirstAnswers, getTagAnswers, getBootstrapAnswers, getRecordingAnswer, entries } from './questions.mjs';
 import chalk from 'chalk';
 import path from 'path';
 import tagger from './tagger.mjs';
 import templator from './templator.mjs';
+import recordings from './recordings.mjs';
 import url from 'url';
 
 const currentFilePath = import.meta.url;
@@ -14,7 +15,8 @@ const filePaths = {
   askConfig: path.resolve(basePath, '..', '.ask', 'config'),
   askConfigTemplate: path.resolve(basePath, '..', '.ask', 'config.template'),
   skillConfig: path.resolve(basePath, '..', 'skill.json'),
-  skillConfigTemplate: path.resolve(basePath, '..', 'skill.template.json')
+  skillConfigTemplate: path.resolve(basePath, '..', 'skill.template.json'),
+  recordings: path.resolve(basePath, '..', 'recordings')
 };
 
 (async () => {
@@ -34,6 +36,9 @@ const filePaths = {
     answers = await getTagAnswers();
 
     await tagger(answers, filePaths);
+  } else if (answers.entry === entries.recordings) {
+    answers = await getRecordingAnswer();
+    recordings(filePaths.recordings, answers);
   }
 
   if (answers.route === 'skill.json') {
