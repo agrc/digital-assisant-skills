66b268726044a6e3ba0b79d8802f717697241244
steveoh <sgourley@utah.gov>
2019-04-30 20:09:40 -0600

    chore: remove alexa items

--- cli/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-exec node --experimental-modules --no-warnings "$(dirname "$0")/$(dirname $(readlink "$0"))/main.mjs" "$@"
--- lambda/py/config/permissions.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# * coding: utf8 *
-'''
-permissions.py
-A module that contains the permissions for the skill
-'''
-
-FULL_ADDRESS = ['alexa::devices:all:address:full:read']
