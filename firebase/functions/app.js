4df11c897c06c3527999637e35252aec8975b524
steveoh <sgourley@utah.gov>
2019-05-08 17:46:30 -0600

    feat: session dates

--- firebase/functions/app.js
+++ firebase/functions/app.js
@@ -277,6 +277,84 @@ app.intent('party statistics', (conv) => {
     .replace('{{rep_percent}}', ((reps / total) * 100).toFixed(1))
   );
 });
+
+app.intent('when is the session', (conv, params) => {
+  // The legislative session always starts in January
+  const JANUARY = 1;
+  // There are 45 days in the session
+  const SESSION_DURATION = 45;
+
+  let year = params.year;
+
+  console.log(`year entity: ${year} as ${typeof (year)}`);
+
+  const today = new Date();
+  const currentYear = today.getFullYear();
+
+  if (!year) {
+    year = currentYear;
+  } else if (typeof (year) === 'string') {
+    year = parseInt(year, 10);
+  }
+
+  const fourthMonday = (year) => {
+    const date = new Date(`${year}/${JANUARY}/1`);
+    let currentMonth = JANUARY;
+    let firstMonday = false;
+    while (currentMonth === JANUARY) {
+      firstMonday = date.getDay() === 1 || firstMonday;
+      date.setDate(date.getDate() + (firstMonday ? 7 : 1));
+      currentMonth = date.getMonth() + 1;
+    }
+
+    date.setDate(date.getDate() - 7);
+
+    return date;
+  };
+
+  const sessionEnds = (date, days) => {
+    let result = new Date(date);
+    result.setDate(result.getDate() + days);
+
+    return result;
+  };
+
+  let start = fourthMonday(year);
+  let end = sessionEnds(start, SESSION_DURATION);
+
+  if (params.kindaordinal) {
+    if (params.kindaordinal === 'next' && today > start) {
+      year++;
+      start = fourthMonday(year);
+      end = sessionEnds(start, SESSION_DURATION);
+    } else if (params.kindaordinal === 'previous' && today < end) {
+      year--;
+      start = fourthMonday(year);
+      end = sessionEnds(start, SESSION_DURATION);
+    }
+  }
+
+  let tense = 'runs';
+  if (today > start) {
+    tense = 'ran';
+  } else if (today < start) {
+    tense = 'will run'
+  }
+
+  let speak = '';
+  if (today > start && today < endsion) {
+    speak = ' is currently in progress and';
+  }
+
+  return conv.ask(text.SESSION
+    .replace('{{year}}', start.toLocaleDateString('en-US', { year: 'numeric' }))
+    .replace('{{inSession}}', speak)
+    .replace('{{tense}}', tense)
+    .replace('{{start}}', start.toLocaleDateString('en-US', { day: 'numeric' }))
+    .replace('{{end}}', end.toLocaleDateString())
+  )
+});
+
 app.intent('Default Welcome Intent', (conv) => conv.ask(text.WELCOME));
 
 module.exports = app;
--- firebase/functions/config/text.js
+++ firebase/functions/config/text.js
@@ -5,4 +5,8 @@ module.exports = {
   AGRC_API_ERROR: 'Make sure your address is in Utah and is valid and try again.',
   LEGISLATOR: 'Your representative is {{rep_party}}, {{rep}} and your Senator is {{sen_party}}, {{sen}}.',
   DETAILS: '{{official}} is a {{profession}} with an education in {{education}}. They became a {{type}} on {{serviceStart}}.',
+  COUNT: 'There are {{total}} total legislators -- with {{sens}} senators and {{reps}} representatives.',
+  COUNT_REPROMPT: 'Do you want to hear the current political party breakdown?',
+  PARTY_STATS: 'There are {{dems}} democrats and {{reps}} republicans. The democrats make up {{dem_percent}} and the republicans {{rep_percent}}.',
+  SESSION: 'The {{year}} legislative session{{inSession}} {{tense}} from January {{start}} to {{end}}.'
 };
