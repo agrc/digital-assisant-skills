5eebfe00c73984de04b237c3f077769f2315bc0e
steveoh <sgourley@utah.gov>
2019-05-09 13:41:50 -0600

    refactor: session intent to own file

--- firebase/functions/app.js
+++ firebase/functions/app.js
@@ -37,88 +37,6 @@ app.intent('specific legislator details', (conv) => {
   return requestLocation(conv, 'To find details about your elected official');
 });
 
-app.intent('when is the session', (conv, params) => {
-  // The legislative session always starts in January
-  const JANUARY = 1;
-  // There are 45 days in the session
-  const SESSION_DURATION = 45;
-
-  let year = params.year;
-
-  console.log(`year entity: ${year} as ${typeof (year)}`);
-
-  const today = new Date();
-  const currentYear = today.getFullYear();
-
-  if (!year) {
-    year = currentYear;
-  } else if (typeof (year) === 'string') {
-    year = parseInt(year, 10);
-  }
-
-  const fourthMonday = (year) => {
-    const date = new Date(`${year}/${JANUARY}/1`);
-    let currentMonth = JANUARY;
-    let firstMonday = false;
-    while (currentMonth === JANUARY) {
-      firstMonday = date.getDay() === 1 || firstMonday;
-      date.setDate(date.getDate() + (firstMonday ? 7 : 1));
-      currentMonth = date.getMonth() + 1;
-    }
-
-    date.setDate(date.getDate() - 7);
-
-    return date;
-  };
-
-  const sessionEnds = (date, days) => {
-    let result = new Date(date);
-    result.setDate(result.getDate() + days);
-
-    return result;
-  };
-
-  let start = fourthMonday(year);
-  let end = sessionEnds(start, SESSION_DURATION);
-
-  if (params.kindaordinal) {
-    if (params.kindaordinal === 'next' && today > start) {
-      year++;
-      start = fourthMonday(year);
-      end = sessionEnds(start, SESSION_DURATION);
-    } else if (params.kindaordinal === 'previous' && today < end) {
-      year--;
-      start = fourthMonday(year);
-      end = sessionEnds(start, SESSION_DURATION);
-    }
-  }
-
-  let tense = 'runs';
-  if (today > start) {
-    tense = 'ran';
-  } else if (today < start) {
-    tense = 'will run'
-  }
-
-  let speak = '';
-  if (today > start && today < end) {
-    speak = ' is currently in progress and';
-  }
-
-  conv.ask(text.SESSION
-    .replace('{{year}}', start.toLocaleDateString('en-US', { year: 'numeric' }))
-    .replace('{{inSession}}', speak)
-    .replace('{{tense}}', tense)
-    .replace('{{start}}', start.toLocaleDateString('en-US', { day: 'numeric' }))
-    .replace('{{end}}', end.toLocaleDateString())
-  );
-
-  return conv.ask(new Suggestions([
-    'When is the next session',
-    'When is the 2050 session'
-  ]));
-});
-
 app.intent('help me', (conv) => {
   conv.ask('I can find out your legislative district, who your legislators are, how many, when the sessions are. Just ask!');
 
@@ -161,7 +79,8 @@ addIntents(
   districtIntent,
   locationReceivedIntent,
   howManyLegislatorsIntent,
-  partyStatisticsIntent
+  partyStatisticsIntent,
+  sessionItent
 );
 
 module.exports = app;
---
+++ firebase/functions/intents/session.js
@@ -0,0 +1,86 @@
+const { Suggestions } = require('actions-on-google');
+const text = require('../config/text');
+
+module.exports = {
+  'when is the session': (conv, params) => {
+    // The legislative session always starts in January
+    const JANUARY = 1;
+    // There are 45 days in the session
+    const SESSION_DURATION = 45;
+
+    let year = params.year;
+
+    console.log(`year entity: ${year} as ${typeof (year)}`);
+
+    const today = new Date();
+    const currentYear = today.getFullYear();
+
+    if (!year) {
+      year = currentYear;
+    } else if (typeof (year) === 'string') {
+      year = parseInt(year, 10);
+    }
+
+    const fourthMonday = (year) => {
+      const date = new Date(`${year}/${JANUARY}/1`);
+      let currentMonth = JANUARY;
+      let firstMonday = false;
+      while (currentMonth === JANUARY) {
+        firstMonday = date.getDay() === 1 || firstMonday;
+        date.setDate(date.getDate() + (firstMonday ? 7 : 1));
+        currentMonth = date.getMonth() + 1;
+      }
+
+      date.setDate(date.getDate() - 7);
+
+      return date;
+    };
+
+    const sessionEnds = (date, days) => {
+      let result = new Date(date);
+      result.setDate(result.getDate() + days);
+
+      return result;
+    };
+
+    let start = fourthMonday(year);
+    let end = sessionEnds(start, SESSION_DURATION);
+
+    if (params.kindaordinal) {
+      if (params.kindaordinal === 'next' && today > start) {
+        year++;
+        start = fourthMonday(year);
+        end = sessionEnds(start, SESSION_DURATION);
+      } else if (params.kindaordinal === 'previous' && today < end) {
+        year--;
+        start = fourthMonday(year);
+        end = sessionEnds(start, SESSION_DURATION);
+      }
+    }
+
+    let tense = 'runs';
+    if (today > start) {
+      tense = 'ran';
+    } else if (today < start) {
+      tense = 'will run'
+    }
+
+    let speak = '';
+    if (today > start && today < end) {
+      speak = ' is currently in progress and';
+    }
+
+    conv.ask(text.SESSION
+      .replace('{{year}}', start.toLocaleDateString('en-US', { year: 'numeric' }))
+      .replace('{{inSession}}', speak)
+      .replace('{{tense}}', tense)
+      .replace('{{start}}', start.toLocaleDateString('en-US', { day: 'numeric' }))
+      .replace('{{end}}', end.toLocaleDateString())
+    );
+
+    return conv.ask(new Suggestions([
+      'When is the next session',
+      'When is the 2050 session'
+    ]));
+  }
+}
