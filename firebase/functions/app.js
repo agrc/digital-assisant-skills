fa9527354855ed8f1880ac2ebbab9b17ac8e0f88
steveoh <sgourley@utah.gov>
2019-05-09 12:11:12 -0600

    refactor: add basic card to welcome

--- firebase/functions/app.js
+++ firebase/functions/app.js
@@ -454,6 +454,20 @@ app.intent('help me', (conv) => {
 app.intent('Default Welcome Intent', (conv) => {
   conv.ask(text.WELCOME);
 
+  conv.ask(new BasicCard({
+    text: text.WELCOME,
+    title: 'Utah Voting Information',
+    subtitle: 'Innovation Grant',
+    buttons: new Button({
+      title: 'Developer Docs',
+      url: 'https://github.com/agrc/digital-assistant-skills/',
+    }),
+    image: new Image({
+      url: 'https://vote.utah.gov/images/header/header-seal.png',
+      alt: 'vote logo',
+    })
+  }));
+
   return conv.ask(new Suggestions([
     'What is my district',
     // 'Who represents me',
